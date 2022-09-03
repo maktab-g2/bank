4f92a4d86a598d58597ee4c088ed17a939bcbc99
Reihaneh Mahjour <rihaneh763@gmail.com>
2022-09-03 10:17:36 +0430

    add Branch, CreditCard and Customer class bodies

--- src/ir/maktab/bank/model/Branch.java
+++ src/ir/maktab/bank/model/Branch.java
@@ -1,4 +1,51 @@
 package ir.maktab.bank.model;
 
 public class Branch {
+    private int id;
+    private String city;
+    private int quality;
+
+    Branch(int id, String city, int quality) {
+        this.id = id;
+        this.city = city;
+        if (quality == 1 || quality == 2 || quality == 3)
+            this.quality = quality;
+        else {
+            System.out.println("The Branch quality out of range. So we replaced it with the lowest amount of quality!");
+            this.quality = 3;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+    public void setQuality(int quality) {
+        if (quality == 1 || quality == 2 || quality == 3)
+            this.quality = quality;
+        else {
+            System.out.println("The Branch quality out of range. So we replaced it with the lowest amount of quality!");
+            this.quality =3;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return city + ", " + quality;
+    }
 }
--- src/ir/maktab/bank/model/CreditCard.java
+++ src/ir/maktab/bank/model/CreditCard.java
@@ -1,4 +1,42 @@
 package ir.maktab.bank.model;
 
 public class CreditCard {
+    private int id;
+    private String cardNumber;
+    private double credit;
+
+    CreditCard(int id, String cardNumber, double credit) {
+        this.id = id;
+        this.cardNumber = cardNumber;
+        this.credit = credit;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public void setCredit(double credit) {
+        this.credit = credit;
+    }
+
+    @Override
+    public String toString() {
+        return cardNumber + ", " + credit;
+    }
 }
