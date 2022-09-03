91616709da1129b6883c5a898b5da9e77cb48a60
Reihaneh Mahjour <rihaneh763@gmail.com>
2022-09-03 16:29:00 +0430

    update LongTermAccountService Class

--- src/ir/maktab/bank/model/Account.java
+++ src/ir/maktab/bank/model/Account.java
@@ -6,6 +6,8 @@ public abstract class Account {
     private CreditCard creditCard;
     private Branch branch;
 
+    private String createAccountDate;
+
     Account(int id, String accountNumber, CreditCard creditCard, Branch branch) {
         this.id = id;
         this.accountNumber = accountNumber;
@@ -44,6 +46,14 @@ public abstract class Account {
 
     public void setBranch(Branch branch) {
         this.branch = branch;
+    }
+
+    public String getCreateAccountDate() {
+        return createAccountDate;
+    }
+
+    public void setCreateAccountDate(String createAccountDate) {
+        this.createAccountDate = createAccountDate;
     }
 
     @Override
--- src/ir/maktab/bank/service/LongTermAccountService.java
+++ src/ir/maktab/bank/service/LongTermAccountService.java
@@ -1,10 +1,22 @@
 package ir.maktab.bank.service;
 
+import ir.maktab.bank.model.Customer;
+import java.time.LocalDate;
+
 public class LongTermAccountService extends AccountService {
+    private double inventory = 10e+3;
     @Override
-    void deposit(String name, int deposit) {
-        System.out.println(name + " deposited " + deposit);
-        total = total + deposit;
-        System.out.println("Balance after deposit: " + total);
+    void deposit(Customer customer, int deposit) {
+        String [] date = customer.getAccount().getCreateAccountDate().split("-");
+        String end = String.valueOf(Integer.parseInt(date[0] ) + 365);
+        end += "-" + date[1] + "-" + date[2];
+        if (String.valueOf(LocalDate.now()).equals(end)) {
+            System.out.println(customer.getName() + " deposited " + deposit);
+            inventory = customer.getAccount().getCreditCard().getCredit() + deposit;
+            System.out.println("Balance after deposit: " + inventory);
+        }
+        else
+            System.out.println(customer.getName() + " can not get money before one year!");
+
     }
 }
