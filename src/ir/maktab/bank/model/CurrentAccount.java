e0fccbf7e7321b2742301c0bfae193bdc85936dc
Shabnam <shabnamghorbani90@gmail.com>
2022-09-03 15:09:41 +0430

    add generate check method

--- src/ir/maktab/bank/model/CurrentAccount.java
+++ src/ir/maktab/bank/model/CurrentAccount.java
@@ -1,4 +1,30 @@
 package ir.maktab.bank.model;
 
-public class CurrentAccount extends Account{
+public class CurrentAccount extends Account {
+
+    private boolean check;
+
+    public void generateCheck(Account account) {
+        if (account instanceof CurrentAccount && account.getCreditCard().getCredit()>=10e+6) {
+            this.check = true;
+        }
+
+
+    }
+
+    public CurrentAccount() {
+    }
+
+    public CurrentAccount(int id, String accountNumber, CreditCard creditCard, Branch branch, boolean check) {
+        super(id, accountNumber, creditCard, branch);
+        this.check = check;
+    }
+
+    public boolean isCheck() {
+        return check;
+    }
+
+    public void setCheck(boolean check) {
+        this.check = check;
+    }
 }
--- src/ir/maktab/bank/model/ShortTermAccount.java
+++ src/ir/maktab/bank/model/ShortTermAccount.java
@@ -1,4 +1,5 @@
 package ir.maktab.bank.model;
 
 public class ShortTermAccount extends Account{
+
 }
